0aaab922be2727182f3fadd04b9ae024f89f3827
Gokul <gokulramalingam1@gmail.com>
2019-11-09 18:52:04 +0530

    Pass in python

---
+++ PassInPython.py
@@ -0,0 +1,13 @@
+# Python Pass
+# In Python, pass keyword is used to execute nothing; 
+# it means, when we don't want to execute code, the pass can be used to execute empty. It is same as the name refers to. 
+# It just makes the control to pass by without executing any code. If we want to bypass any code pass statement can be used.
+
+# Python Pass Syntax
+
+# pass
+
+for i in [1,2,3,4,5]:  
+    if i==3:  
+        pass  #If condition is true then do nothing>Therefore we use pass to execute nothing
+    print(i) 

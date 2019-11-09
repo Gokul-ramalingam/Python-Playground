1f1fd023efc46d3c6a4867785cf2f809f7833ff2
Gokul <gokulramalingam1@gmail.com>
2019-11-09 12:55:24 +0530

    Literals in python

---
+++ literals.py
@@ -0,0 +1,62 @@
+# Python Literals
+# Literals can be defined as a data that is given in a variable or constant.
+
+# Python support the following literals:
+
+# I. String literals:
+# String literals can be formed by enclosing a text in the quotes. We can use both single as well as double quotes for a String.
+
+# Eg: "Aman" , '12345'
+
+# Types of Strings:
+
+# There are two types of Strings supported in Python:
+# a).Single line String- Strings that are terminated within a single line are known as Single line Strings.
+
+text = 'hello'
+
+# b).Multi line String- A piece of text that is spread along multiple lines is known as Multiple line String.
+
+# There are two ways to create Multiline Strings:
+
+# 1). Adding black slash at the end of each line.
+
+text1 = 'hello\
+    Python'
+
+# 2).Using triple quotation marks:-
+text2 = '''welcome 
+to 
+SSSIT'''  
+
+print(text)
+print(text1)
+print(text2)
+
+
+# II.Numeric literals:
+
+# Numeric Literals are immutable. Numeric literals can belong to following four different numerical types.
+# Int(signed integers)	Long(long integers)	float(floating point)	Complex(complex)
+
+# III. Boolean literals:
+
+# A Boolean literal can have any of the two values: True or False.
+
+
+# IV. Special literals.
+
+# Python contains one special literal i.e., None.
+
+# None is used to specify to that field that is not created. It is also used for end of lists in Python.
+
+val1 = 10
+val2 = None
+
+print(val1)
+print(val2)
+
+
+# V.Literal Collections.
+
+# Collections such as tuples, lists and Dictionary are used in Python.

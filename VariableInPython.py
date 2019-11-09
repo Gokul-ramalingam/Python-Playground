a48bf46f2753c115c62e1215b8689b2f6283f89d
Gokul <gokulramalingam1@gmail.com>
2019-11-09 12:54:03 +0530

    Variables in python

---
+++ VariableInPython.py
@@ -0,0 +1,19 @@
+# The first character of the variable must be an alphabet or underscore ( _ ).
+# All the characters except the first character may be an alphabet of lower-case(a-z), upper-case (A-Z), underscore or digit (0-9).
+# Identifier name must not contain any white-space, or special character (!, @, #, %, ^, &, *).
+# Identifier name must not be similar to any keyword defined in the language.
+# Identifier names are case sensitive for example my name, and MyName is not the same.
+# Examples of valid identifiers : a123, _n, n_9, etc.
+# Examples of invalid identifiers: 1a, n%4, n 9, etc.
+
+# Declaring Variable and Assigning Values:
+# Python does not bound us to declare variable before using in the application. It allows us to create variable at required time.
+
+a = 10
+name = "Doe"
+salary = 2000000
+
+print(a)
+print(name)
+print(salary)
+

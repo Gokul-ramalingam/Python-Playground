3b4e318a43502d83681970c4bc5baa6177ad69b7
Gokul <gokulramalingam1@gmail.com>
2019-11-09 12:54:54 +0530

    Multiple assignment in python

---
+++ MultipleAssignment.py
@@ -0,0 +1,22 @@
+# Multiple Assignment
+# Python allows us to assign a value to multiple variables in a single statement 
+# which is also known as multiple assignment.
+
+# We can apply multiple assignments in two ways either by assigning a single value 
+# to multiple variables or assigning multiple values to multiple variables. Lets see given examples.
+
+# 1. Assigning single value to multiple variables
+
+x=y=z=7
+
+print(x)
+print(y)
+print(z)
+
+# 2.Assigning multiple values to multiple variables:
+# The values will be assigned in the order in which variables appears.
+
+a,b,c = 10,15,20
+print(a)
+print(b)
+print(c)

7188a35c1faa9b70862301a70516f39a91f97f06
Gokul <gokulramalingam1@gmail.com>
2019-11-11 10:30:51 +0530

    Union of two sets in python

---
+++ UnionOfTwoSets.py
@@ -0,0 +1,16 @@
+# Union of two Sets
+# The union of two sets are calculated by using the or (|) operator. 
+# The union of the two sets contains the all the items that are present in both the sets.
+
+# Example 1 : using union | operator
+Days1 = {"Monday","Tuesday","Wednesday","Thursday"}  
+Days2 = {"Friday","Saturday","Sunday"}  
+print(Days1|Days2) #printing the union of the sets
+
+# Python also provides the union() method which can also be used to calculate the union of two sets.
+# Example 2: using union() method
+Days1 = {"Monday","Tuesday","Wednesday","Thursday"}  
+Days2 = {"Friday","Saturday","Sunday"}  
+print(Days1.union(Days2)) #printing the union of the sets 
+
+
